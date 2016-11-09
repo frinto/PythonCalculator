974ef90511735fc0409af1bde2d91b95aebb0030
frinto <huyxkhoa@gmail.com>
2016-11-09 15:24:42 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,7 @@ if x == 2 or y == "watsup dan":
     x = x + 1
     y = "hello world"
 
-    #testing from github
-    #as0vja09sjv0a9sjv
-    #teatiaosgioajsgassssssss test 3
-
-    
-    #just another test from github made a new user account to see if i can push.
-    
-    #IT WORKS WOOT
-    
-    #i am a test user!
-
-    #test121212
-
-    #from test user
+    #test pull
     
 
 
