9c8c09d039a8946be0457885cecc6e5a58c904a6
Frinto <frinto@riseup.net>
2016-11-14 16:29:04 -0800

    UPDATING SOURCE CODE

--- calculator.py
+++ calculator.py
@@ -39,10 +39,10 @@ def multiply(x, y):
 
 ''' Main Program '''
 actions = {
-    '-add': addition,
-    '-subtract': substract,
-    '-divide': divide,
-    '-multiply': multiply
+    'add': addition,
+    'subtract': substract,
+    'divide': divide,
+    'multiply': multiply
 }
 
 
@@ -50,7 +50,7 @@ def EntryPoint():
 
     #if length of arguments does not equal 4 then write error requiring correct number of input.
     if (len(sys.argv) != 4):
-        raise CalculatorException('Usage: calculator.py <switch> <number> <number>')
+        raise CalculatorException('Usage: calculator.py <switch> <leftHandSideNumber> <rightHandSideNumber>')
 
     #if the action is not recognized as a valid switch then print error
     #sys.argv[1] represents if its division, multiplication, addition, or subtraction
@@ -58,16 +58,21 @@ def EntryPoint():
         raise CalculatorException('%s is not a recognized switch!' % sys.argv[1])
 
     #convert the value into a float
-    #sys.argv[2] means the left value example calculator.py add 5 20
+    #example python calculator.py add 5 20
+    #sys.argv[0] will represent FILE NAME "calculator.py"
+    #sys.argv[1] will represent the switch which is either dividing, multiplying, adding, or subtracting
     #sys.argv[2] will represent the value 5
-    #sys.argv[3] will represent the value 20
+    #sys.argv[3] represents 20 will represent the value 20
+
     leftHandSide = float(sys.argv[2])
     rightHandSide = float(sys.argv[3])
 
     #sys.argv[0] represents the name of the file for example printing sys.argv[0] will show this file name is calculator.py
-    print(("script name = "), sys.argv[1])
+    print("python file name = " + sys.argv[0])
 
-
+    #outputting our result
+    #the action that will be performed on leftHandSide and rightHandSide
+    print("result of " + " " +sys.argv[2] + " " + sys.argv[1] + " " + sys.argv[3] + " = ")
     print(actions[sys.argv[1]](leftHandSide, rightHandSide))
 
 
